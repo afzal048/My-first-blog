7d0ffbad60fb9a22bc7312c58640a0adb8ecd485
afzal <afzalahmad1336@gmail.com>
2016-10-26 11:43:40 +0530

    A simple blog Devoloped

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,8 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
